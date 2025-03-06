105123b9e4b0db0666db51eaffa7a333c11d0778
damiancodes <damianbwire88@gmail.com>
2025-03-06 14:37:50 +0300

    commit

--- employee/models.py
+++ employee/models.py
@@ -5,6 +5,8 @@ class Employee(models.Model):
     ename = models.CharField(max_length=100)
     eemail = models.EmailField()
     econtact = models.CharField(max_length=15)
+    edate_of_admission = models.DateField(null=True, blank=True)
+
 
     class Meta:
         db_table = "employee"
